b99a70818c8a4d0c34a935b9cfef6896843a80f5
AKingbolade SHada <gboladeshada@gmail.com>
2018-09-29 00:42:48 +0100

    Initial Commit

---
+++ safetygenie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  safetygenie
+//
+//  Created by Akingbolade Shada on 9/29/18.
+//  Copyright © 2018 Akingbolade Shada. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
